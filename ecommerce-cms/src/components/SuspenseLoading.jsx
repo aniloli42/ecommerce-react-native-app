631d0814bbdf206141c6509b34039a7ecc8266b4
aniloli42 <aniloli42@gmail.com>
2022-04-11 13:37:05 +0545

    Loader Styled

--- ecommerce-cms/src/components/SuspenseLoading.jsx
+++ ecommerce-cms/src/components/SuspenseLoading.jsx
@@ -1,11 +1,17 @@
 import { Suspense } from 'react';
 
 const SuspenseLoading = ({ children }) => (
-  <Suspense fallback={<Loading />}>{children}</Suspense>
+  <Suspense fallback={<Loader />}>{children}</Suspense>
 );
 
-const Loading = () => {
-  return <p className="text-white">Loading</p>;
+const Loader = () => {
+  return (
+    <div className="max-w-xl self-center p-6">
+      <p className="text-gray-300 bg-gray-800 p-6 rounded-md">
+        Page Loading...
+      </p>
+    </div>
+  );
 };
 
 export default SuspenseLoading;
